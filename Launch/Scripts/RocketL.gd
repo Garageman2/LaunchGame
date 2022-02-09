9934f15e3dbb1382890c0b099355d8efe57ffdab
Garageman2 <61953092+Garageman2@users.noreply.github.com>
2022-02-09 09:04:38 -0800

    content

---
+++ Launch/Scripts/RocketL.gd
@@ -0,0 +1,33 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+var RocketPos = Vector2(0,0)
+onready var LaunchRoot = get_parent();
+export var Radius = 300
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	LaunchRoot.set("position",Vector2(0,get_viewport_rect().size.y))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	RocketPos = get_global_mouse_position();
+	RocketPos = RocketPos - LaunchRoot.get("position")
+	set("position",FindPosition(RocketPos))
+	return
+
+
+func FindPosition(RocketPos):
+	var NewCoords = null;
+	NewCoords = cartesian2polar(RocketPos.x,RocketPos.y)
+	print(NewCoords)
+	NewCoords.x = clamp(NewCoords.x,75,Radius)
+	NewCoords.y = clamp(NewCoords.y,-1.5,-.2)
+	NewCoords = polar2cartesian(NewCoords.x,NewCoords.y)
+	#theta can be used to calculate the forward vector for the motion
+	#R can be used for x for the spring force.
+	return NewCoords;

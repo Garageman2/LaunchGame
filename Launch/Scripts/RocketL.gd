94f243d7a106fc72b40c0f4e5b6d7e3f6e71d05f
Garageman2 <61953092+Garageman2@users.noreply.github.com>
2022-02-12 10:04:56 -0800

    Added some sprites and working on spring scaling

--- Launch/Scripts/RocketL.gd
+++ Launch/Scripts/RocketL.gd
@@ -1,5 +1,7 @@
 extends Sprite
 
+onready var Parent = get_parent();
+onready var Spring = Parent.get_child(1)
 
 # Declare member variables here. Examples:
 var RocketPos = Vector2(0,0)
@@ -12,6 +14,7 @@ signal Launched
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	LaunchRoot.set("position",Vector2(0,get_viewport_rect().size.y))
+	print(Spring)
 	pass # Replace with function body.
 
 
@@ -30,10 +33,14 @@ func _process(delta):
 func FindPosition(RocketPos):
 	var NewCoords = null;
 	NewCoords = cartesian2polar(RocketPos.x,RocketPos.y)
-	print(NewCoords)
 	NewCoords.x = clamp(NewCoords.x,75,Radius)
 	NewCoords.y = clamp(NewCoords.y,-1.5,-.2)
+	Spring.set("rotation_degrees",rad2deg(NewCoords.y)+90)
+	set("rotation_degrees",rad2deg(NewCoords.y)+90)
+	Spring.set("scale",Vector2(.3,NewCoords.x*.003))
+	
 	NewCoords = polar2cartesian(NewCoords.x,NewCoords.y)
+
 	#theta can be used to calculate the forward vector for the motion
 	#R can be used for x for the spring force.
 	return NewCoords;

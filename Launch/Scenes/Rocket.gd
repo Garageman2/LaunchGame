dd6d911abd968a68d6fdcc834a13c6b0aa88b008
Garageman2 <61953092+Garageman2@users.noreply.github.com>
2022-02-13 10:22:25 -0800

    Launch Particles

--- Launch/Scenes/Rocket.gd
+++ Launch/Scenes/Rocket.gd
@@ -5,11 +5,14 @@ var Acceleration = -9.81
 var Mass = 20.0
 var SpringConst = .8
 var InFlight = false
+var Emitting = true
+onready var Fire = $Sprite/FireParticles
+onready var Smoke = $Sprite/Smoke
 onready var Cam = $Camera2D
 onready var MyTween = $Tween
 
 func PhysicsSetup(ForcePolar, OldCam):
-	Velocity = polar2cartesian(.5*SpringConst*(450-ForcePolar.x)*(1/Mass) * 4,ForcePolar.y)
+	Velocity = polar2cartesian(.5*SpringConst*(450-ForcePolar.x)*(1/Mass) * 6,ForcePolar.y)
 	InFlight = true
 	Cam.set_global_position(OldCam.get("position"))
 	Cam.make_current()
@@ -25,6 +28,11 @@ func _physics_process(delta):
 		set("position",get("position") + (Velocity))
 		set("rotation_degrees", rad2deg(cartesian2polar(Velocity.x,Velocity.y).y)+90)
 		Velocity.y -= delta*Acceleration
+		print(Velocity.y)
+		if Velocity.y > 0:
+			Emitting = false
+			Fire.set("emitting",Emitting)
+			Smoke.set("emitting",Emitting)
 		if get("position").y >  get_viewport_rect().size.y - 40:
 			InFlight = false
 	pass
